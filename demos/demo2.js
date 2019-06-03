5de183952f4edc96ce55411142667e7066a3a1fe
mac <1010479088@qq.com>
2019-06-03 21:30:20 +0800

    1.JavaScript 初步试探

--- demos/demo2.js
+++ demos/demo2.js
@@ -1,5 +1,10 @@
 // var age = 10;
 // var name = 'abc';
 // console.log(typeof (typeof age), typeof (typeof name));
-var x = 5, y = 6;
-console.log(x + ++y);
+
+// var x = 5, y = 6;
+// console.log(x + ++y);
+
+var a = 5;
+var b = '5';
+console.log(a !== b);
